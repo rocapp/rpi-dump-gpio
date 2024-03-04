19a2e8310f0acf2d17377edb2762862ce3ac1ef6
Robbie Capps <rocapp@gmail.com>
2024-03-04 15:17:03 -0800

    v0.1.1

--- rpi_dump_gpio/main.py
+++ rpi_dump_gpio/main.py
@@ -3,18 +3,34 @@ import pigpio
 import tabulate
 import pandas as pd
 import numpy as np
+import time
+import sys
+import click
 
-PORTS = (17, 19, 21, 23, 24, 25)
+PINS = (17, 19, 21, 23, 24, 25)
 
 
-def main():
+@click.group(invoke_without_command=True)
+@click.option('-p', '--pins', default=PINS, type=click.INT, multiple=True,
+              help='GPIO pins to monitor.')
+def main(pins):
     pi = pigpio.pi()
-    df = pd.DataFrame(np.zeros((1,len(PORTS))), columns=PORTS)
+    print('Initialized PiGPIO instance.')
+    
+    df = pd.DataFrame(np.zeros((1,len(pins))), columns=pins)
+    
     def read_pin(p: int):
         return pi.read(int(p))
-    
+
+    def read_pins():
+        df = df.apply(lambda el: read_pin(int(el.column)), )
+        return df
+
+    input('[Press any key to continue...]')
+    print('Continuously monitoring GPIO values...')
+    time.sleep(1)
     while True:
-        df = df.apply(lambda el: read_pin(int(el.column)), axis=1)
+        df = read_pins()
         print(df.to_markdown())
 
 
