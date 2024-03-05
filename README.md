d1bf2e9ebdc35327de602b7ec83d8b1d65678dcb
Robbie Capps <rocapp@gmail.com>
2024-03-04 17:27:01 -0800

    updates from z38s

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # rpi-dump-gpio
-script to dump raspberry pi gpio values.
+
+Script to dump raspberry pi gpio values.
+
+## Installation
+
+```bash
+    $ poetry install
+```
+
+## Usage
+
+### CLI
+
+```bash
+    $ rpi-dump-gpio --help
+Usage: rpi-dump-gpio [OPTIONS] COMMAND [ARGS]...
+
+Options:
+-p, --pins INTEGER  GPIO pins to monitor.
+--help              Show this message and exit.
+```
+
+```bash
+    $ rpi-dump-gpio --p 17 -p 19 -p 21 -p 23 -p 24 -p 25
+```
--- rpi_dump_gpio/main.py
+++ rpi_dump_gpio/main.py
@@ -7,6 +7,11 @@ import numpy as np
 import time
 import sys
 import click
+from dotenv import load_dotenv, find_dotenv
+
+loaded_env = load_dotenv(find_dotenv(), override=True)
+
+PIGPIO_HOST = os.getenv('PIGPIO_HOST', 'localhost')
 
 PINS = (17, 19, 21, 23, 24, 25)
 
@@ -14,12 +19,23 @@ PINS = (17, 19, 21, 23, 24, 25)
 @click.group(invoke_without_command=True)
 @click.option('-p', '--pins', default=PINS, type=click.INT, multiple=True,
               help='GPIO pins to monitor.')
-def main(pins):
-    pi = pigpio.pi()
-    print('Initialized PiGPIO instance.')
-    
-    df = pd.DataFrame(dict(zip(pins, -np.zeros((1,len(pins))))), columns=pins)
-    
+@click.option('-H', '--host', default=PIGPIO_HOST, type=click.STRING,
+              help='Raspberry Pi Host IP.')
+@click.option('-P', '--port', default=8888, type=click.INT, help='Raspberry Pi Port.')
+def main(pins, host, port):
+    print(f'Starting rpi-dump-gpio (at {host}:{port})...')
+    pi = pigpio.pi(host=host, port=port)
+    print('Initialized PiGPIO instance on {}:{}.'.format(
+        host, port) if pi.connected else 'Failed to connect.')
+
+    if not pi.connected:
+        exit(1)
+
+    if not pins:
+        exit(0)
+
+    df = pd.DataFrame(dict(zip(pins, -np.zeros((1, len(pins))))), columns=pins)
+
     def read_pin(p: int):
         return pi.read(int(p))
 
@@ -36,4 +52,3 @@ def main(pins):
 
 if __name__ == '__main__':
     main()
-
